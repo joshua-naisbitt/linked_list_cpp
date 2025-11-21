48070067b0c7c9ddd06e4ba45117bc10ab731f86
Josh <74276429+joshua-naisbitt@users.noreply.github.com>
2025-11-21 15:58:20 -0600

    Fix whitespace after Node constructor

--- linked_list.cpp
+++ linked_list.cpp
@@ -9,7 +9,7 @@ class LinkedList {
         // Node Constructor                                                      
         Node() : data(0), next(nullptr){}                                        
         Node(int val) : data(val), next(nullptr){}                               
-    }                                                                            
+    }                                           
                                                                                  
 public:                                                                          
     // LinkedList Constructor                                                    
